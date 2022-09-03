66dc73b3a1e8b360d940f67e00d72107b9fc01b9
Aleyna <arslankayaaleyna@gmail.com>
2022-09-03 13:02:44 +0300

    Initial Commit

---
+++ Walkino/WalkinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkinoApp.swift
+//  Walkino
+//
+//  Created by Aleyna Arslankaya on 3.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WalkinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
